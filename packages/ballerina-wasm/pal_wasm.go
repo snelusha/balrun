051047ab67b15e219ca9dc88b14b66cd3b9159e8
Sithija Nelusha Silva <hello@snelusha.dev>
2026-08-04 05:19:14 +0530

    refactor: encapsulate fs operations in palFS

--- packages/ballerina-wasm/pal_wasm.go
+++ packages/ballerina-wasm/pal_wasm.go
@@ -10,6 +10,7 @@ import (
 	"io/fs"
 	"path"
 	"strings"
+	"sync"
 	"syscall/js"
 	"time"
 )
@@ -212,16 +213,22 @@ func (c *fetchHTTPClient) extractBody(resp js.Value) ([]byte, error) {
 	return body, nil
 }
 
-func resolvePath(cwd string, p string) string {
+type palFS struct {
+	cwd  string
+	fsys *bridgeFS
+	mu   sync.Mutex
+}
+
+func (f *palFS) resolvePath(p string) string {
 	if path.IsAbs(p) {
 		return p
 	}
-	return path.Join(cwd, p)
+	return path.Join(f.cwd, p)
 }
 
-func createParentDirs(fsys *bridgeFS, p string) error {
+func (f *palFS) createParentDirs(p string) error {
 	dir := path.Dir(p)
-	info, err := fs.Stat(fsys, dir)
+	info, err := fs.Stat(f.fsys, dir)
 	if err == nil {
 		if !info.IsDir() {
 			return &fs.PathError{Op: "mkdirAll", Path: dir, Err: fs.ErrInvalid}
@@ -229,39 +236,55 @@ func createParentDirs(fsys *bridgeFS, p string) error {
 		return nil
 	}
 	if errors.Is(err, fs.ErrNotExist) {
-		return fsys.MkdirAll(dir, 0o755)
+		return f.fsys.MkdirAll(dir, 0o755)
 	}
 	return err
 }
 
+func (f *palFS) readFile(p string) ([]byte, error) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	return fs.ReadFile(f.fsys, f.resolvePath(p))
+}
+
+func (f *palFS) writeFile(p string, data []byte) error {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	resolvedPath := f.resolvePath(p)
+	if err := f.createParentDirs(resolvedPath); err != nil {
+		return err
+	}
+	return f.fsys.WriteFile(resolvedPath, data, 0o644)
+}
+
+func (f *palFS) appendFile(p string, data []byte) error {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	resolvedPath := f.resolvePath(p)
+	if err := f.createParentDirs(resolvedPath); err != nil {
+		return err
+	}
+	current, err := fs.ReadFile(f.fsys, resolvedPath)
+	if err != nil && !errors.Is(err, fs.ErrNotExist) {
+		return err
+	}
+	return f.fsys.WriteFile(resolvedPath, append(current, data...), 0o644)
+}
+
 func newPal(cwd string, fsys *bridgeFS, stdout, stderr io.Writer, signals pal.SignalSource) pal.Platform {
+	palFS := &palFS{cwd: cwd, fsys: fsys}
+
 	return pal.Platform{
 		IO: pal.IO{
 			Stdout: stdout.Write,
 			Stderr: stderr.Write,
 		},
 		FS: pal.FS{
-			ReadFile: func(p string) ([]byte, error) {
-				return fs.ReadFile(fsys, resolvePath(cwd, p))
-			},
-			WriteFile: func(p string, data []byte) error {
-				resolvedPath := resolvePath(cwd, p)
-				if err := createParentDirs(fsys, resolvedPath); err != nil {
-					return err
-				}
-				return fsys.WriteFile(resolvedPath, data, 0o644)
-			},
-			AppendFile: func(p string, data []byte) error {
-				resolved := resolvePath(cwd, p)
-				if err := createParentDirs(fsys, resolved); err != nil {
-					return err
-				}
-				current, err := fs.ReadFile(fsys, resolved)
-				if err != nil && !errors.Is(err, fs.ErrNotExist) {
-					return err
-				}
-				return fsys.WriteFile(resolved, append(current, data...), 0o644)
-			},
+			ReadFile:   palFS.readFile,
+			WriteFile:  palFS.writeFile,
+			AppendFile: palFS.appendFile,
 		},
 		HTTP: pal.HTTP{
 			NewClient: func(cfg pal.ClientConfig) pal.HTTPClient {
