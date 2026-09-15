9cef77d5e95b22ac2bbe8f8575f39e81ac00e1c8
Sithija Nelusha Silva <hello@snelusha.dev>
2026-09-16 03:18:44 +0530

    refactor: simplify source handling in diagnostic printer

--- packages/ballerina-wasm/main_wasm.go
+++ packages/ballerina-wasm/main_wasm.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/ballerina-nutcracker/ballerina/lib/rt"
-	"github.com/ballerina-nutcracker/ballerina/platform/pal"
-	"github.com/ballerina-nutcracker/ballerina/projects"
-	"github.com/ballerina-nutcracker/ballerina/runtime"
-	"github.com/ballerina-nutcracker/ballerina/tools/diagnostics"
 	"io"
 	"io/fs"
 	"os"
 	"path"
 	"syscall/js"
+
+	_ "github.com/ballerina-nutcracker/ballerina/lib/rt"
+	"github.com/ballerina-nutcracker/ballerina/platform/pal"
+	"github.com/ballerina-nutcracker/ballerina/projects"
+	"github.com/ballerina-nutcracker/ballerina/runtime"
+	"github.com/ballerina-nutcracker/ballerina/tools/diagnostics"
 )
 
 func main() {
@@ -132,14 +133,14 @@ func run(_ js.Value, args []js.Value) any {
 		}
 
 		if diags := result.Diagnostics(); diags.HasErrors() {
-			printDiagnostics(fsys, runPath, stderr, diags, diagnostics.NewDiagnosticEnv(), opts.noColors)
+			printDiagnostics(stderr, diags, diagnostics.NewDiagnosticEnv(), opts.noColors)
 			resolve.Invoke(1)
 			return
 		}
 
 		compilation := result.Project().CurrentPackage().Compilation()
 		if diags := compilation.DiagnosticResult(); diags.HasErrors() {
-			printDiagnostics(fsys, runPath, stderr, diags, compilation.DiagnosticEnv(), opts.noColors)
+			printDiagnostics(stderr, diags, compilation.DiagnosticEnv(), opts.noColors)
 			resolve.Invoke(1)
 			return
 		}
